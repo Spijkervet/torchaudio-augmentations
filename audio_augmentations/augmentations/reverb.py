a0e4ae98c6b1437e465652fa3f1d3c525bbfdd2a
jspijkervet <janne.spijkervet@gmail.com>
2021-02-06 13:43:55 +0100

    changed reverb

--- audio_augmentations/augmentations/reverb.py
+++ audio_augmentations/augmentations/reverb.py
@@ -1,26 +1,30 @@
 import random
 import augment
+import torch.nn as nn
 
-
-class Reverb:
-    def __init__(self, sr, p=0.5):
+class Reverb(nn.Module):
+    def __init__(self, sr, reverberance_min=0, reverberance_max=100, dumping_factor_min=0, dumping_factor_max=100, room_size_min=0, room_size_max=100, p=0.5):
         self.sr = sr
+        self.reverberance_min = reverberance_min
+        self.reverberance_max = reverberance_max
+        self.dumping_factor_min = dumping_factor_min
+        self.dumping_factor_max = dumping_factor_max
+        self.room_size_min = room_size_min
+        self.room_size_max = room_size_max
         self.p = p
-        self.reverberance = lambda: random.randint(1, 100)  # 0 - 100
-        self.dumping_factor = lambda: random.randint(1, 100)  # 0 - 100
-        self.room_size = lambda: random.randint(1, 100)  # 0 - 100
-        self.effect_chain = (
-            augment.EffectChain()
-            .reverb(self.reverberance, self.dumping_factor, self.room_size)
-            .channels(1)
-        )
         self.src_info = {"rate": self.sr}
         self.target_info = {
             "channels": 1,
             "rate": self.sr,
         }
 
-    def __call__(self, audio):
+    def forward(self, audio):
+        self.effect_chain = (
+            augment.EffectChain()
+            .reverb(self.reverberance, self.dumping_factor, self.room_size)
+            .channels(1)
+        )
+
         if random.random() < self.p:
             audio = self.effect_chain.apply(
                 audio, src_info=self.src_info, target_info=self.target_info
