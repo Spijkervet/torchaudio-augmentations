836b49b69e020b52e137050fc82de805204332e6
Janne Spijkervet <janne.spijkervet@bytedance.com>
2021-06-29 18:03:51 +0100

    updated pip version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/spijkervet/torchaudio-augmentations"
 EMAIL = "janne.spijkervet@gmail.com"
 AUTHOR = "Janne Spijkervet"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.6"
+VERSION = "0.2.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["numpy", "torch", "torchaudio", "julius", "wavaugment"]
