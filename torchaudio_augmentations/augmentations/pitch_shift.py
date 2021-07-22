416471ca51f507b17e83142d902231f98dfb7a2d
Janne <janne.spijkervet@gmail.com>
2021-07-22 18:15:10 +0100

    Fixed pitch_shift.py if y sample is too large

--- torchaudio_augmentations/augmentations/pitch_shift.py
+++ torchaudio_augmentations/augmentations/pitch_shift.py
@@ -32,9 +32,9 @@ class PitchShift:
 
         if y.shape[1] != audio.shape[1]:
             if y.shape[1] > audio.shape[1]:
-                y = y[:, audio.shape[1]]
+                y = y[:, : audio.shape[1]]
             else:
-                y0 = torch.zeros(1, audio.shape[1])
+                y0 = torch.zeros(1, audio.shape[1]).to(y.device)
                 y0[:, : y.shape[1]] = y
                 y = y0
         return y
